7c5d8729e6354e169b2ddd770676734af81f55cd
Fakie <68382320+Projectmaster122@users.noreply.github.com>
2024-05-23 17:38:19 +0000

    first commit

---
+++ src/CounterScript.svelte
@@ -0,0 +1,50 @@
+<script>
+    // our export lets (props)
+    export let value = 0; // doubled value in case yes.
+    export let toDouble = false;
+    export let clicks = 0;
+    export let pClass = "text-lg text-gray-600 mb-8";
+    export let buttonClass = "bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75";
+    export let toCenter = true;
+
+
+    //funcs
+    function onclick() {
+        value = value * 2;
+        clicks = clicks + 1;
+    }
+
+</script>
+
+{#if toCenter === true}
+    <center>
+       <br>
+       <br>
+       <br>
+       <br>
+       <button on:click={onclick} class={buttonClass}>
+           Increment
+       </button>
+
+       {#if toDouble != false}
+           <br>
+           <p class={pClass} >You have clicked {clicks}, times! Thats {value} Doubled!</p>
+       {:else}
+           <br>
+           <p class={pClass} >You have clicked {clicks} times!</p>
+       {/if}
+    </center>
+{:else}
+    <button on:click={onclick} class={buttonClass}>
+        Increment
+    </button>
+
+    {#if toDouble != false}
+        <br>
+        <p class={pClass} >You have clicked {clicks}, times! Thats {value} Doubled!</p>
+    {:else}
+        <br>
+        <p class={pClass} >You have clicked {clicks} times!</p>
+    {/if}
+
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import CounterScript from "../CounterScript.svelte"
+    let pkg = {
+        "value": 2,
+        "clicks": 1
+    }
+    // has to be separate from pkg because funni
+    let toDouble = false;
+    function svetlog() {
+        if (toDouble) {
+            toDouble = false
+        }else {
+            toDouble = true;
+        }
+        
+    }
+</script>
+
+<CounterScript bind:toDouble={toDouble} {...pkg}/>
+
+<button on:click={svetlog}>
+    toDouble = {toDouble}
+</button>
