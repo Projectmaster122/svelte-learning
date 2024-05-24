d84b3ac9fc3b748c4f8ba12d2201575dfcab9da5
Fakie <68382320+Projectmaster122@users.noreply.github.com>
2024-05-24 19:45:29 +0000

    funni

--- src/CounterScriptAbout.svelte
+++ src/CounterScriptAbout.svelte
@@ -1,6 +1,12 @@
 <script>
     let count = 0;
-
+    let funny_visible = false;
+    $: {
+        if (count == 10) {
+            count = 0;
+            funny_visible = true;
+        }
+    }
     function onclick() {
         count += 1;
     }
@@ -10,3 +16,9 @@
 <button on:click={onclick} class="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-7">
     Your count: {count}
 </button>
+
+<br>
+
+{#if funny_visible}
+    Told you it was worse.
+{/if}
